c3495bef3ed4214d2a72605e06de5fe3ddac53bc
alindberg999 <alindberg999@gmail.com>
2020-11-20 16:37:15 -0500

    Added feedback atributes and constructor

--- src/main/java/edu/wpi/cs3733/b20/kotlin/demo/model/Feedback.java
+++ src/main/java/edu/wpi/cs3733/b20/kotlin/demo/model/Feedback.java
@@ -1,5 +1,12 @@
 package edu.wpi.cs3733.b20.kotlin.demo.model;
 
 public class Feedback {
-
+	User user;
+	long timestamp;
+	String text;
+	
+	//should this be User user or string user
+	public Feedback (User user, long timestamp, String text) {
+		
+	}
 }
