0dfae2188ddbdf874947cbf31b1c2169132ac293
alindberg999 <alindberg999@gmail.com>
2020-11-20 16:42:02 -0500

    Constructor of feedback

--- src/main/java/edu/wpi/cs3733/b20/kotlin/demo/model/Feedback.java
+++ src/main/java/edu/wpi/cs3733/b20/kotlin/demo/model/Feedback.java
@@ -1,12 +1,17 @@
 package edu.wpi.cs3733.b20.kotlin.demo.model;
 
+import java.sql.Date;
+
 public class Feedback {
 	User user;
 	long timestamp;
 	String text;
 	
-	//should this be User user or string user
 	public Feedback (User user, long timestamp, String text) {
-		
+		this.user = user;
+		this.timestamp = timestamp;
+		this.text = text;
 	}
+	
+	
 }
