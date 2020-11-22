e550a46a281b1568ec66797e2b62dec6273f12e6
Josh <Josh@130.215.123.32>
2020-11-22 15:25:09 -0500

    default Alternative constructor to make AWS happy hopefully

--- src/main/java/edu/wpi/cs3733/b20/kotlin/demo/model/Alternative.java
+++ src/main/java/edu/wpi/cs3733/b20/kotlin/demo/model/Alternative.java
@@ -10,6 +10,10 @@ public class Alternative{
 	public ArrayList<Approval> approvals = new ArrayList<>();;
 	public ArrayList<Disapproval> disapprovals = new ArrayList<>();
 	
+	//default constructor to make AWS happy
+	public Alternative() {
+		
+	}
 	
 	public Alternative(String description) {
 		this.description = description;
