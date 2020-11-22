b098e00aafacfb8dde66e8a0413d6b64d29735bd
cagoss85 <cagoss85@gmail.com>
2020-11-22 13:52:13 -0500

    CreateUserRequestClass

--- src/main/java/edu/wpi/cs3733/b20/kotlin/demo/http/CreateUserRequest.java
+++ src/main/java/edu/wpi/cs3733/b20/kotlin/demo/http/CreateUserRequest.java
@@ -1,5 +1,18 @@
 package edu.wpi.cs3733.b20.kotlin.demo.http;
 
 public class CreateUserRequest {
-
+	public String username;
+	public String password;
+	public boolean hasPassword;
+	
+	public CreateUserRequest(String username, String password, boolean hasPassword) {
+		this.username = username;
+		this.password = password;
+		this.hasPassword = hasPassword;
+	}
+	
+	public CreateUserRequest(String username, boolean hasPassword) {
+		this.username = username;
+		this.hasPassword = hasPassword;
+	}
 }
