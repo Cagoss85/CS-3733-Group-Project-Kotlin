3e3ab8a1108726aca60707c7cd19b7aef32f14f7
cagoss85 <cagoss85@gmail.com>
2020-11-29 16:15:35 -0500

    Created handler class and function for getting all choices

---
+++ src/main/java/edu/wpi/cs3733/b20/kotlin/demo/GetReportHandler.java
@@ -0,0 +1,46 @@
+package edu.wpi.cs3733.b20.kotlin.demo;
+
+import java.util.ArrayList;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import edu.wpi.cs3733.b20.kotlin.demo.db.ChoicesDAO;
+import edu.wpi.cs3733.b20.kotlin.demo.http.GetReportResponse;
+import edu.wpi.cs3733.b20.kotlin.demo.model.Choice;
+
+public class GetReportHandler implements RequestHandler<Object, GetReportResponse>{
+	LambdaLogger logger;
+	
+	//Searches RDS for all constants
+	public ArrayList<Choice> getChoices() throws Exception{
+		logger.log("in getChoices");
+		ChoicesDAO dao = new ChoicesDAO();
+		
+		return dao.getAllChoices();
+	}
+	
+	/*
+	 * Function returns an array of all choices currently found in the system. 
+	 */
+	@Override
+	public GetReportResponse handleRequest(Object input, Context context) {
+		logger = context.getLogger();
+		logger.log("Generating a report of all choices.");
+		
+		GetReportResponse response;
+		try {
+			ArrayList<Choice> reportList = getChoices();
+
+			response = new GetReportResponse(reportList);
+		} catch (Exception e) {
+			response = new GetReportResponse(400, e.getMessage());
+		}
+		return response;
+	}
+
+
+
+	
+}
--- src/main/java/edu/wpi/cs3733/b20/kotlin/demo/http/GetReportResponse.java
+++ src/main/java/edu/wpi/cs3733/b20/kotlin/demo/http/GetReportResponse.java
@@ -7,12 +7,21 @@ import edu.wpi.cs3733.b20.kotlin.demo.model.Choice;
 public class GetReportResponse {
 	public ArrayList<Choice> choiceReport;
 	
+	public int statusCode;
+	public String error;
+	
 	public GetReportResponse(ArrayList<Choice> choiceReport) {
 		this.choiceReport = choiceReport;
+		this.statusCode = 200;
+		this.error = "";
+	}
+	
+	public GetReportResponse(int code, String errorMsg) {
+		this.statusCode = code;
+		this.error = errorMsg;
 	}
 	
 	public GetReportResponse() {
-		
 	}
 	
 	public ArrayList<Choice> getReport(){return this.choiceReport;}
