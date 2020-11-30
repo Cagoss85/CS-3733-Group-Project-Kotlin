76202547214436b2234bb1c758bbd52cc253bf5e
alindberg999 <alindberg999@gmail.com>
2020-11-30 15:39:08 -0500

    Fix Body of AdminConsole and added xhr function to getReport

--- js/getReport.js
+++ js/getReport.js
@@ -8,5 +8,28 @@ function getReport(){
 	
 	xhr.send();
 	
-		console.log("sent");
+	console.log("sent");
+		
+	xhr.onloadend = function(){
+		console.log(xhr);
+		if(xhr.readyState == XMLHttpRequest.DONE){
+			if(xhr.status == 200){
+					console.log ("XHR:" + xhr.responseText);
+					processGetChoiceResponse(xhr.responseText);
+				} else{
+					console.log("actual:" + xhr.responseText);
+					var js = JSON.parse(xhr.responseText);
+					var err = js["error"];
+					alert (err);
+				}
+		} else{
+			console.log("res:" + xhr.responseText);
+			
+			document.getElementById("customURL").value = window.location.href;  //TODO: COMMENT THIS OUT LATER
+			
+			//document.getElementById("alt1").innerHTML = "Report not found."
+			
+			
+		}
+	}
 }
