e5cff227dc6786a8cb192b2fec0b2dcc1fcf8ef9
alindberg999 <alindberg999@gmail.com>
2020-12-03 17:02:55 -0500

    More Coverage for the User Class

--- src/test/java/edu/wpi/cs3733/b20/kotlin/demo/TestUser.java
+++ src/test/java/edu/wpi/cs3733/b20/kotlin/demo/TestUser.java
@@ -1,6 +1,8 @@
 package edu.wpi.cs3733.b20.kotlin.demo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -22,5 +24,18 @@ public class TestUser {
 		User user1 = new User("uuid", "user1","password1");
 		
 		assertEquals(user1.toString(), "Choice UUID: uuid Username: user1 No password.");
+	}
+	
+	@Test 
+	public void testUserEquals() {
+		User user1 = new User("uuid", "user1","password1");
+		User user2 = new User("uuid", "user1","password1");
+		User user3 = new User ("uuid", "different UserName","password1");
+		User user4 = new User ("different UUID", "user1","password1");
+		assertFalse(user1.equals(null));
+		assertTrue(user1.equals(user2));
+		assertFalse(user1.equals(user3));
+		assertFalse(user1.equals(user4));
+		
 	}
 }
