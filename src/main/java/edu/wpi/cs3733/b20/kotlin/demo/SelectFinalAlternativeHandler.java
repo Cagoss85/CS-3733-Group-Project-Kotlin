148be2787b80f0ebf3ca96db559555a8766aa16f
Arnold-Muralt <awmuralt@wpi.edu>
2020-12-10 16:52:25 -0500

    updates to Final alternative handler to refelect DB struct

--- src/main/java/edu/wpi/cs3733/b20/kotlin/demo/SelectFinalAlternativeHandler.java
+++ src/main/java/edu/wpi/cs3733/b20/kotlin/demo/SelectFinalAlternativeHandler.java
@@ -1,26 +1,19 @@
 package edu.wpi.cs3733.b20.kotlin.demo;
 
-import java.util.ArrayList;
-import java.util.UUID;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import edu.wpi.cs3733.b20.kotlin.demo.db.ChoicesDAO;
-import edu.wpi.cs3733.b20.kotlin.demo.http.CreateChoiceRequest;
-import edu.wpi.cs3733.b20.kotlin.demo.http.CreateChoiceResponse;
 import edu.wpi.cs3733.b20.kotlin.demo.http.SelectFinalAlternativeRequest;
 import edu.wpi.cs3733.b20.kotlin.demo.http.SelectFinalAlternativeResponse;
-import edu.wpi.cs3733.b20.kotlin.demo.model.Alternative;
-import edu.wpi.cs3733.b20.kotlin.demo.model.Choice;
 
 public class SelectFinalAlternativeHandler implements RequestHandler<SelectFinalAlternativeRequest,SelectFinalAlternativeResponse>{
 	LambdaLogger logger;
 	ChoicesDAO choiceDAO = new ChoicesDAO();
-	Boolean SelectFinalAlternative(String uuid, Alternative alternative,int altID) {
+	Boolean SelectFinalAlternative(String uuid,int altID) {
 		try {
-			return choiceDAO.addFinalAlternative(uuid, alternative, altID);
+			return choiceDAO.addFinalAlternative(uuid, altID);
 		}catch(Exception e) {
 			return false;
 		}
